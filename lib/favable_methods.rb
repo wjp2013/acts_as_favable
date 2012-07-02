0b1fc5adc9c3acd221208dc37297e530579a43ac
Victor Wang <wjp2013@gmail.com>
2012-07-02 17:33:23 +0800

    Removed call to AR::Base.class_name_of_active_record_descendant.

--- lib/favable_methods.rb
+++ lib/favable_methods.rb
@@ -17,12 +17,12 @@ module Acts #:nodoc:
     
     module SingletonMethods
       def find_favorites_for(obj)
-        favable = ActiveRecord::Base.send(:class_name_of_active_record_descendant, self).to_s
+        favable = self.base_class.name.to_s
         Favorite.find_favorites_for_favable(favable, obj.id)
       end
       
       def find_favorites_by_user(user) 
-        favable = ActiveRecord::Base.send(:class_name_of_active_record_descendant, self).to_s
+        favable = self.base_class.name.to_s
         Favorite.where(["user_id = ? and favable_type = ?", user.id, favable]).order("created_at DESC")
       end
     end
